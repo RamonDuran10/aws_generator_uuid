9df614829cd596a178476c2d211e040e04794b32
Ramon Duran <ramonjosedg@hotmail.com>
2022-07-16 16:23:04 -0400

    project inializated

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require ('express');
+const http = require ('http');
+const helmet = require ('helmet');
+let compression = require ('compression');
+require ('dotenv').config();
+const {v4:uuidv4 } = require('uuid');
+
+const app = express();
+app.use(helmet({contentSecurityPolicy: false}));
+app.use(compression());
+
+const serverHttp = http.createServer(app);
+serverHttp.listen(process.env.HTTP_PORT, process.env.IP); 
+
+app.use(express.static('./public'));
+
+app.get('/api/get-uuid', function (req, res ){
+    res.send(uuidv4());
+});
+app.get('*', function (req, res ){
+    res.status(404).send('Erro 404 - Resource not found.');
+});
